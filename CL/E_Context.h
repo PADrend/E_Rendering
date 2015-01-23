ccaa40ad34f726817407e681a4c67d3b7073b952
MeisterYeti <myeti@mail.upb.de>
2015-01-23 16:27:09 +0100

    added missing license headers

--- CL/E_Context.h
+++ CL/E_Context.h
@@ -1,3 +1,11 @@
+/*
+	This file is part of the Rendering library.
+	Copyright (C) 2014-2015 Sascha Brandt <myeti@mail.upb.de>
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 #ifdef RENDERING_HAS_LIB_OPENCL
 
 #ifndef E_RENDERING_CL_E_CONTEXT_H_
