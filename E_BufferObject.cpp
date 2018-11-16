9042ea1ad506e4cf8e0d306d6d1c460fb8c437b8
Sascha Brandt <myeti@mail.uni-paderborn.de>
2018-11-16 15:36:51 +0100

    fixed wrong constants in BufferObject.clear

--- E_BufferObject.cpp
+++ E_BufferObject.cpp
@@ -221,8 +221,8 @@ void E_BufferObject::init(EScript::Namespace & lib) {
 		return a;
 	})
 	
-	//! [ESMF] thisEObj BufferObject.clear(bufferTarget, [internalFormat=GL_R8, format=GL_RED, type=GL_BYTE])
-	ES_MFUN(typeObject,BufferObject,"clear",1,4,(thisObj->clear(parameter[0].toUInt(), parameter[1].toUInt(0x8229), parameter[2].toUInt(0x1903), parameter[3].toUInt(0x1400)), thisEObj))
+	//! [ESMF] thisEObj BufferObject.clear(bufferTarget, [internalFormat=GL_R8UI, format=GL_RED_INTEGER, type=GL_UNSIGNED_BYTE])
+	ES_MFUN(typeObject,BufferObject,"clear",1,4,(thisObj->clear(parameter[0].toUInt(), parameter[1].toUInt(0x8232), parameter[2].toUInt(0x8D94), parameter[3].toUInt(0x1401)), thisEObj))
 		
 	//! [ESMF] thisEObj BufferObject._bind(bufferTarget, [location])
 	ES_MFUN(typeObject,BufferObject,"_bind",1,2,(parameter.count() == 1 ? thisObj->bind(parameter[0].toUInt()) : thisObj->bind(parameter[0].toUInt(), parameter[1].toUInt()), thisEObj))
